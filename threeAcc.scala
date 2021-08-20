4c5d0a8d8fe430b44cc6e656895d43f3dfc7f1be
Imal-Kovinda <83854345+Imal-Kovinda@users.noreply.github.com>
2021-08-20 09:57:13 +0530

    Transfer

---
+++ threeAcc.scala
@@ -0,0 +1,27 @@
+package act_3
+
+class Account{
+  var acc1=8000;
+  var acc2 = 2000;
+  //var acc2=0;
+  
+  def transfer(trn:Double):Double={
+    return acc1 - trn;
+  }
+  def nowblnce(trn:Double):Double={
+    return acc2 + trn;
+  }
+}
+
+object threeAcc {
+  def main(args:Array[String]){
+    var p1 = new Account();
+    var p2 = new Account();
+    
+    print("Enter Transfer amount: ");
+    var trns = scala.io.StdIn.readInt();
+    println("Current value of Account 1: " + p1.transfer(trns));
+    println("Current value of Account 2: " + p2.nowblnce(trns));
+  }
+ 
+}
